cf64e8e8c5bd32ba3f7a35fabeeb801f762762c7
MrLoyd10 <jhonloydocoy@gmail.com>
2024-04-17 22:10:53 +0800

    update routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/dashboard.vue'
 import Login from '../views/login.vue'
+import Home from '../views/home.vue'
 
 const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
     {
         path: '/dashboard',
         name: 'dashboard',
